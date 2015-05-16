7bcee39f19974f981175b3d200361391834662f0
ismael-miguel <espertalhao04@hotmail.com>
2015-05-16 22:40:06 +0100

    (Attempt to) Improve readability

--- js/highlight.js
+++ js/highlight.js
@@ -1,59 +1,63 @@
 (function(window){
 	
-	var f=window.highlight=function(lang,element){
+	var f=window.highlight = function(lang, element){
 
-		var lang_defs=f[lang];
+		var lang_defs = f[lang];
 		
 		if(!lang_defs)
 		{
-			throw new TypeError('The language "'+lang+'" wasn\'t included');
+			throw new TypeError( 'The language "' + lang + '" wasn\'t included' );
 		}
 		
-		if(!(element instanceof Element ))
+		if(!(element instanceof Element))
 		{
-			throw new TypeError('The 2nd parameter must be an Element');
+			throw new TypeError( 'The 2nd parameter must be an Element' );
 		}
 		
-		element.className+=' highlight '+lang;
+		element.className += ' highlight ' + lang;
 		
-		for(var i=0,l=lang_defs.length;i<l;i++)
+		for(var i = 0, l = lang_defs.length; i<l; i++)
 		{
-			var html='';
+			var html = '';
 			
-			for(var j=0,k=element.childNodes.length;j<k;j++)
+			for(var j = 0, k = element.childNodes.length; j<k; j++)
 			{
 				if(element.childNodes[j].nodeType === 3)
 				{
-					html+=element.childNodes[j].nodeValue
+					html += element.childNodes[j].nodeValue
 						.replace(
 							lang_defs[i].match,
-							//'string' === typeof lang_defs[i].replace || 'function' === typeof lang_defs[i].replace
-							{'string':1,'function':1}[typeof lang_defs[i].replace]
+							/*shortcut to decide if the lang_defs[i].replace is one of those types
+							 *if so, passes it directly
+							 *otherwise, makes a string matching based on the object
+							 */
+							{'string':1, 'function':1}[ typeof lang_defs[i].replace ]
 								? lang_defs[i].replace
-								: '<'+lang_defs[i].replace.tag+
-									' class="'+lang_defs[i]['class']+'">'+
-										lang_defs[i].replace.text+
-									'</'+lang_defs[i].replace.tag+'>'
+								: '<' + lang_defs[i].replace.tag +
+									' class="' + lang_defs[i]['class'] + '">' +
+										lang_defs[i].replace.text +
+									'</' + lang_defs[i].replace.tag + '>'
 						);
 				}
 				else
 				{
-					html+=element.childNodes[j].outerHTML;
+					html += element.childNodes[j].outerHTML;
 				}
 			}
-			element.innerHTML=html;
+			element.innerHTML = html;
 			
 			if('function' === typeof lang_defs[i].patch)
 			{
-				var returned=lang_defs[i].patch.call(element);
+				var returned = lang_defs[i].patch.call( element );
 				if('string' === typeof returned)
 				{
-					element.innerHTML=returned;
+					element.innerHTML = returned;
 				}
 			}
 		}
 	};
 
-	f.default_replace={tag:'span',text:'$1'};
+	//default replace object
+	f.default_replace = {tag:'span', text:'$1'};
 
-})(Function('return this')());
+})(Function('return this')());//just be sure that we have the real window
