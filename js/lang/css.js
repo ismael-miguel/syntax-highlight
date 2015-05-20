4905623f89649613bf7aee982ad8306140cda31a
ismael-miguel <espertalhao04@hotmail.com>
2015-05-20 01:47:54 +0100

    Plenty of fixed, and new styles

--- js/lang/css.js
+++ js/lang/css.js
@@ -15,13 +15,34 @@
 				'replace':'[<span class="attribute">$1</span>'
 			},
 			{
-				 'match':/(-?[a-z][a-z\-]+):(?:([^;}]+)(?=;|}))?/g,
+				'match':/(-?[a-z][a-z\-]+):(?:([^;}]+)(?=;|}))?/gi,
 				'replace':'<span class="style">$1</span>:<span class="value">$2</span>'
 			},
 			{
 				'class':'comment',
-				'match':/(\/\*(?:[^*]|\*[^\/])*\*\/)/g,
-				'replace':window.highlight.default_replace
+				'match':/(\/\*(?:[^*]|\*[^\/])*(?:\*\/|$))/g,
+				'replace':window.highlight.default_replace,
+				'patch':function(){
+					return this.innerHTML.replace(//matches multi-line comments
+							/<span class="comment">(\/\*(?:[^*]|\*[^\/])+(?:\*\/(?:<\/span>)?|$))/g,
+							function(_,part1){
+								return '<span class="comment">'+
+									//cleans up all spans
+									(part1||'').replace(/<\/?span(?: class="[^"]+")?>/g,'')+
+									'</span>';
+							}
+						);
+				}
+			},
+			{
+				'class':'at-rule',
+				'match':/@(-?[a-z\-\_]+)/gi,
+				'replace':'@<span class="at-rule">$1</span>'
+			},
+			{
+				'class':'pseudo',
+				'match':/:(:?)(-?[a-z\-\_]+)/gi,
+				'replace':':$1<span class="pseudo">$2</span>'
 			},
 			{
 				'class':'class',
