9ca20abab099bca229f4f449aca3d52c9c29cb63
ismael-miguel <espertalhao04@hotmail.com>
2015-05-20 01:48:36 +0100

    Reverted accidental overwrite.

--- js/lang/sql.js
+++ js/lang/sql.js
@@ -2,59 +2,78 @@
 
 	if('function' === typeof window.highlight)
 	{
-		
-		window.highlight.langs.css=[
+		window.highlight.langs.sql=[
 			{
 				'class':'string',
-				'match':/("(?:[^"]|[\\"]")*"|'(?:[^']|[\\']')*')(?=[\b\s\),;\/\]]|$)/g,
+				'match':/([bn]?"(?:[^"]|[\\"]")*"|[bn]?'(?:[^']|[\\']')*')(?=[\b\s\(\),;\$#\+\-\*\/]|$)/g,
 				'replace':window.highlight.default_replace
 			},
 			{
-				'class':'attribute',
-				'match':/\[([a-z\-\_]+)(?=\s*(?:[\|*\^\$\~]?=|\]))/gi,
-				'replace':'[<span class="attribute">$1</span>'
-			},
-			{
-				'match':/(-?[a-z][a-z\-]+):(?:([^;}]+)(?=;|}))?/gi,
-				'replace':'<span class="style">$1</span>:<span class="value">$2</span>'
-			},
-			{
 				'class':'comment',
-				'match':/(\/\*(?:[^*]|\*[^\/])*(?:\*\/|$))/g,
+				'match':/((?:\/\/|\-\-\s|#)[^\r\n]*|\/\*(?:[^*]|\*[^\/])*(?:\*\/|$))/g,
 				'replace':window.highlight.default_replace,
 				'patch':function(){
-					return this.innerHTML.replace(//matches multi-line comments
+					return this.innerHTML.replace(
+							/((?:\/\/|\-\-\s|#)[^\r\n]*|\/\*(?:[^*]|\*[^\/])*(?:\*\/|$))/g,
+							'$1</span>'
+						).replace(//matches single-line comments
+							/<span class="comment">((?:#|-- |\/\/)(?:.|<\/span><span class="[^"]+">([^<])<\/span>)*)([\r\n]|$)/g,
+							function(_,part1,part2,part3){
+								return '<span class="comment">'+
+									//cleans up all spans
+									((part1||'')+(part2||'')).replace(/<\/?span(?: class="[^"]+")?>/g,'')+
+									'</span>'+
+									(part3||'');
+							}
+						).replace(//matches multi-line comments
 							/<span class="comment">(\/\*(?:[^*]|\*[^\/])+(?:\*\/(?:<\/span>)?|$))/g,
 							function(_,part1){
 								return '<span class="comment">'+
 									//cleans up all spans
-									(part1||'').replace(/<\/?span(?: class="[^"]+")?>/g,'')+
+									((part1||'')).replace(/<\/?span(?: class="[^"]+")?>/g,'')+
 									'</span>';
 							}
 						);
 				}
 			},
 			{
-				'class':'at-rule',
-				'match':/@(-?[a-z\-\_]+)/gi,
-				'replace':'@<span class="at-rule">$1</span>'
+				/*    
+				 * numbers aren't that 'regular' and many edge-cases were left behind    
+				 * with the help of @MLM (http://stackoverflow.com/users/796832/mlm),    
+				 * we were able to make this work.    
+				 * he took over the regex and patched it all up, I did the replace string    
+				 */
+				'match':/((?:^|\b|\(|\s|,))(?![a-z_]+)([+\-]?\d+(?:\.\d+)?(?:[eE]-?\d+)?)((?=$|\b|\s|\(|\)|,|;))/g,
+				'replace':'$1<span class="number">$2</span>$3'
 			},
 			{
-				'class':'pseudo',
-				'match':/:(:?)(-?[a-z\-\_]+)/gi,
-				'replace':':$1<span class="pseudo">$2</span>'
+				'class':'name',
+				'match':/(`[^`]+`)/g,
+				'replace':window.highlight.default_replace
 			},
 			{
-				'class':'class',
-				'match':/\.([a-z\-\_]+)(?=\s*|{|\.)/gi,
-				'replace':'.<span class="class">$1</span>'
+				'class':'var',
+				'match':/(@@?[a-z_][a-z_\d]*)/g,
+				'replace':window.highlight.default_replace
 			},
 			{
-				'class':'tag',
-				'match':/([a-z]+|\*)(?=[:\s\.,\[\{])/g,
+				'class':'keyword',
+				//the keyword replace must have an aditional check (`(?!\()` after the name), due to the function replace()
+				'match':/\b(accessible|add|all|alter|analyze|and|as|asc|asensitive|before|between|bigint|binary|blob|both|by|call|cascade|case|change|char|character|check|collate|column|condition|constraint|continue|convert|create|cross|current_date|current_time|current_timestamp|current_user|cursor|database|databases|day_hour|day_microsecond|day_minute|day_second|dec|decimal|declare|default|delayed|delete|desc|describe|deterministic|distinct|distinctrow|div|double|drop|dual|each|else|elseif|enclosed|escaped|exists|exit|explain|false|fetch|float|float4|float8|for|force|foreign|from|fulltext|generated|get|grant|group|having|high_priority|hour_microsecond|hour_minute|hour_second|if|ignore|in|index|infile|inner|inout|insensitive|insert|int|int1|int2|int3|int4|int8|integer|interval|into|io_after_gtids|io_before_gtids|is|iterate|join|key|keys|kill|leading|leave|left|like|limit|linear|lines|load|localtime|localtimestamp|lock|long|longblob|longtext|loop|low_priority|master_bind|master_ssl_verify_server_cert|match|maxvalue|mediumblob|mediumint|mediumtext|middleint|minute_microsecond|minute_second|mod|modifies|natural|nonblocking|not|no_write_to_binlog|null|numeric|on|optimize|optimizer_costs|option|optionally|or|order|out|outer|outfile|parse_gcol_expr|partition|precision|primary|procedure|purge|range|read|reads|read_write|real|references|regexp|release|rename|repeat|replace(?!\()|require|resignal|restrict|return|revoke|right|rlike|schema|schemas|second_microsecond|select|sensitive|separator|set|show|signal|smallint|spatial|specific|sql|sqlexception|sqlstate|sqlwarning|sql_big_result|sql_calc_found_rows|sql_small_result|ssl|starting|stored|straight_join|table|terminated|then|tinyblob|tinyint|tinytext|to|trailing|trigger|true|undo|union|unique|unlock|unsigned|update|usage|use|using|utc_date|utc_time|utc_timestamp|values|varbinary|varchar|varcharacter|varying|virtual|when|where|while|with|write|xor|year_month|zerofill)\b/gi,
+				'replace':window.highlight.default_replace
+			},
+			{
+				'class':'func',
+				'match':/\b([a-z_][a-z_\d]*)\b(?=\()/gi,
+				'replace':window.highlight.default_replace
+			},
+			{
+				'class':'name',
+				'match':/\b([a-z\_][a-z_\d]*)\b/gi,
 				'replace':window.highlight.default_replace
 			}
 		];
 	}
 
 })(Function('return this')());
+
